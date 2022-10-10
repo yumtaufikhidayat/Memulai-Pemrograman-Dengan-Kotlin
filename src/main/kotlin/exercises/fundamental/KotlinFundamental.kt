01f52dde0da148050bbe0fd2d970a12fc4457306
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-10-11 06:14:31 +0700

    Update fundamental

--- src/main/kotlin/exercises/fundamental/KotlinFundamental.kt
+++ src/main/kotlin/exercises/fundamental/KotlinFundamental.kt
@@ -1,5 +1,18 @@
 package exercises.fundamental
 
+// single line comment
+/* main function */
 fun main() {
     println("Halo dunia!")
+    name()
+}
+/*
+* multi line comment
+*/
+fun name() {
+    val name = "Taufik Hidayat"
+
+    print("Hello my name is ")
+    println(name)
+    print(if (true) "Always true" else "Always false")
 }
